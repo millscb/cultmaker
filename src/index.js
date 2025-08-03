ca4235144fec3378ed65c156ca97f0b289e12d20
millscb <40242984+millscb@users.noreply.github.com>
2025-08-03 00:03:47 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,31 @@
+/* ===== src/index.js ===== */
+import "./styles.css";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import CultMakerApp from "./CultMakerApp";
+
+const MODULE_ID = "cultmaker";
+
+class CultMakerWindow extends Application {
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      id: "cultmaker-window",
+      title: "CultMaker",
+      width: 920,
+      height: 600,
+      resizable: true,
+      template: `modules/${MODULE_ID}/templates/cultmaker.html`,
+    });
+  }
+
+  /** Render then mount React */
+  async _render(...args) {
+    await super._render(...args);
+    const container = this.element.find("#cultmaker-root")[0];
+    if (container && !container._reactRoot) {
+      container._reactRoot = createRoot(container);
+      container._reactRoot.render(<CultMakerApp />);
+    }
+    return this;
+  }
+}
